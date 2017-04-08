a9befaac43d5e960309c0fe834ac6e24ff639631
Morten la Cour <lacour@gmail.com>
2017-04-08 20:43:49 +0200

    Added Tuples

---
+++ VS2017Templates.App/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace VS2017Templates.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person("Morten", "la Cour");
+
+
+            //Try tuples!!!
+            var fullName = p.GetFullName();
+            Console.WriteLine($"Hello {fullName.Item1} {fullName.Item2}");
+
+            var fullNameStrong = p.GetStrongFullName();
+            Console.WriteLine($"Hello {fullNameStrong.firstName} {fullNameStrong.lastName}");
+
+
+
+        }
+    }
+
+
+    public class Person
+    {
+        private string _firstName;
+        private string _lastName;
+
+        public Person (string firstName, string lastName) {
+            _firstName = firstName;
+            _lastName = lastName;
+        }
+
+        public (string,string) GetFullName()
+        {
+            return (_firstName, _lastName);
+        }
+
+        public (string firstName, string lastName) GetStrongFullName()
+        {
+            return (_firstName, _lastName);
+        }
+
+
+    }
+}
